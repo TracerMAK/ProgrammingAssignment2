c99124105e747654e5c318aa08abbfd6017b5e6f
Mark <tsmckiel@live.com>
2020-06-29 22:17:29 -0500

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Stores a matrix and allows for the getting/setting of its value and the
+## value of its inverse.
 
-## Write a short comment describing this function
+library(digest)
 
+## Creates a list that contains four separate functions that sets/gets the value
+## of a matrix and sets/gets the value of the inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inv) inverse <<- inv
+    getinverse <- function() inverse
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## Creates a global key/value map cache where the key is the hash value of the
+## matrix and the value is the inverse of that matrix. The intention is to
+## calculate the inverse of an unique matrix one time, as long as the 
+## 'inversemap' object persists.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    if (!exists("inversemap")) {
+        inversemap <<- list(matrix())
+    }
+    
+    inverse <- x$getinverse()
+    if (is.null(inverse)) {
+        key <- digest(x$get())
+        if (is.null(inversemap[[key]])) {
+            print("Updating cache...")
+            inversemap[[key]] <<- solve(x$get())
+        }
+        x$setinverse(inversemap[[key]])
+    }    
+    x$getinverse()
 }
